56723c1f573378a79205cf769c2d31e470d727d8
majdoub1rayen <139793543+majdoub1rayen@users.noreply.github.com>
2023-12-21 22:16:03 +0100

    db and login backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connexion = require("./app/controllers/db").connexion;
+app.use(cors());
+app.use(express.json());
+
+connexion.connect((err) => {
+  if (err) {
+    console.error("error", err);
+    return;
+  }
+  console.log("Connected to database");
+});
+const saveUser = require("./app/controllers/user/user.controller");
+saveUser(app); // Include the user routes
+// Include your product routes
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../service/user.service';
-import { User } from '../model/user';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
-  userName: string = '';
+export class DashboardComponent {
 
-  constructor(private userService: UserService) {}
-
-  ngOnInit(): void {
-    // Get the user's email from local storage
-    const userEmail = localStorage.getItem('email');
-
-    if (userEmail) {
-      // Fetch all users and filter by the user's email
-      this.userService.getAllUsers().subscribe((users: User[]) => {
-        const userWithEmail = users.find((user) => user.email === userEmail);
-        if (userWithEmail) {
-          this.userName = `${userWithEmail.First_Name} ${userWithEmail.Last_name}`;
-        }
-      });
-    }
-  }
 }

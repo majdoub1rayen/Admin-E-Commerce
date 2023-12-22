a41e5c957567e4bf03fbde37063861265e37403e
majdoub1rayen <139793543+majdoub1rayen@users.noreply.github.com>
2023-12-22 15:40:25 +0100

    sidebar button

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+declare const $: any;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  toggleSidebar() {
+    $('#sidebar').toggleClass('active');
+  }
 }
